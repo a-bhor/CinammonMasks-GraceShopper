f775cae6356be4c719e1e8e7ace958cccac2c2bb
Torrel <taurel107@gmail.com>
2020-11-14 11:46:28 -0500

    feat(client): switching to main branch

--- client/app.js
+++ client/app.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
-import {Navbar} from './components'
 import Routes from './routes'
-import Home from './components/home'
 import Footer from './components/footer'
 
 const App = () => {
   return (
     <div id="main">
       <Routes />
-      {/* <Home /> */}
       <Footer />
     </div>
   )
